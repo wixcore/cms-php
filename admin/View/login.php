194e342210d85bdb5b5d4912556e4138f49ca497
ua.lifesheets <ua.lifesheets@gmail.com>
2024-01-03 01:46:11 +0200

    Create login.php

---
+++ admin/View/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.ico">
+    <title>Войти в WixCMS</title>
+    <link href="/admin/Assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/admin/Assets/css/login.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container">
+
+    <form class="form-signin" role="form">
+        <h2 class="form-signin-heading">Войти в WixCMS</h2>
+        <input type="email" class="form-control" placeholder="Электронная почта" required autofocus>
+        <input type="password" class="form-control" placeholder="Пароль" required>
+        <label class="checkbox">
+            <input type="checkbox" value="remember-me"> Запомнить меня
+        </label>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Авторизоваться</button>
+    </form>
+</div>
+
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+
+</body>
+
+</html>
